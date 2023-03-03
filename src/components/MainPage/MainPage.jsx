68f2af0499ce6519b2fcd0bc10c13395d7589e0b
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-03 12:37:14 -0500

    add button ui

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './MainPage.module.css';
+import Button from '../UI/Button';
 
 function MainPage() {
   return (
     <div className={styles.main_page}>
       <h1>Welcome to My Website</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut tortor non purus dictum consectetur. Maecenas faucibus elit ut elit consequat, in faucibus arcu sodales. Vestibulum tincidunt, augue quis commodo mollis, quam justo ullamcorper magna, in molestie nunc nibh et nisl. Quisque eget mi ut odio varius faucibus eget vel urna.</p>
-      <button>Learn More</button>
+      <Button>Learn More</Button>
     </div>
   );
 }
