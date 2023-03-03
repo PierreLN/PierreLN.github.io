fbbdae6c7e58d47443416ed0ac941ce9bdd3dab6
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-03 11:16:04 -0500

    test page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter, Route, Link } from "react-router-dom";
+import "./App.css";
 // import SideMenu from './components/SideMenu/SideMenu';
 // import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      {/* <SideMenu></SideMenu>
-      <MainPage></MainPage> */}
-      helloooo
-      this is a test page
-      6
-    </div>
+    <HashRouter basename="/">
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </HashRouter>
   );
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 export default App;
