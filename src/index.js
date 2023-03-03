7833eadc898dc1aff00de477c402527c58483abf
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-03 10:43:37 -0500

    test page

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+
+ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
