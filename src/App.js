87112b26124858b05ec4b274ba5e321c8215f52f
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-02 20:59:55 -0500

    first page up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import SideMenu from './components/SideMenu/SideMenu';
-import MainPage from './components/MainPage/MainPage';
+// import SideMenu from './components/SideMenu/SideMenu';
+// import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
