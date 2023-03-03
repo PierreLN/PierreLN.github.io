44b3e5530bd97514c292eb7ea96cd00dd6e2c7b7
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-03 11:13:15 -0500

    test page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
