08d45bb0a30f0e4fa443d4fa7f95a7faee7b9f79
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-03-31 11:46:02 -0400

    add πr

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -17,7 +17,7 @@ function HomePage(props) {
           Welcome to my porfolio website!
           <p>...made with React</p>
           <p className={styles.name}>
-            {signstart} I'm πr (Pierre) {signEnd}
+            {signstart}πr{signEnd} I'm Pierre {signstart}πr{signEnd}
           </p>
         </div>
         <div className={styles.presentation} onClick={onClickHandler}>
