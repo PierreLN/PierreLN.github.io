3834904399f354bdaaa3bb2a7781e856fa9e5da2
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-05-29 15:53:42 -0400

    test

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -69,7 +69,7 @@ function MainPage({ onClick, selectedPage }) {
       }
     }
 
-    if (!isCounted && localStorage.getItem('Counted')) {
+    if (!isCounted) {
       return () => visitorAdded();
     }
   }, []);
