9470b3e3e36d7f7bd9232b76831372f8f5fa0d90
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2023-05-29 10:51:06 -0400

    css messagecontact

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -12,7 +12,7 @@ function ContactPage() {
     email: "",
     message: "",
   });
-  const [isSent, setIsSent] = useState(false)
+  const [isSent, setIsSent] = useState(false);
   const nameRef = useRef();
   const emailRef = useRef();
   const messageRef = useRef();
@@ -30,11 +30,10 @@ function ContactPage() {
           },
         }
       );
-      setIsSent(true)
+      setIsSent(true);
     } catch (error) {
       console.log(error);
     }
-
   };
 
   const onChangeHandler = () => {
@@ -57,6 +56,89 @@ function ContactPage() {
     }
   };
 
+  let content = (
+    <div className={styles.contact_me_section}>
+      <form
+        onSubmit={onSubmitHanlder}
+        encType="text/plain"
+        className={styles.form_main_box}
+      >
+        <div className={styles.form_section}>
+          <h1>Contact Me</h1>
+          <label className={styles.name} htmlFor="name">
+            Full Name
+          </label>
+          <input
+            className={styles.name_input}
+            type="text"
+            name="name"
+            id="name"
+            ref={nameRef}
+            onChange={onChangeHandler}
+          ></input>
+          <label className={styles.email} htmlFor="email">
+            Email
+          </label>
+          <input
+            className={styles.email_input}
+            ref={emailRef}
+            type="email"
+            name="email"
+            id="email"
+            onChange={onChangeHandler}
+          ></input>
+          <label className={styles.message} htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className={styles.message_input}
+            type="text"
+            name="message"
+            id="message"
+            placeholder="Any questions?"
+            ref={messageRef}
+            onChange={onChangeHandler}
+          ></textarea>
+        </div>
+        <div>
+          <button className={styles.button_section} type="submit">
+            Send
+          </button>
+        </div>
+      </form>
+      <div className={styles.info_section}>
+        <div className={styles.info}>
+          <div className={styles.info_email}>
+            <h2>Email</h2>
+            <div className={styles.info_email_text}>{email}</div>
+          </div>
+
+          <div className={styles.info_location}>
+            <h2>Based in</h2>
+            <div className={styles.info_location_text}>
+              <div>Montreal</div>
+              <div>Quebec</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.media} onClick={socialMediaHandler}>
+          <Card className={styles.contactPage_media}>LinkedIn</Card>
+          <Card className={styles.contactPage_media}>Github</Card>
+        </div>
+      </div>
+    </div>
+  );
+
+  if (isSent) {
+    content = (
+      <div className={styles.thanks}>
+        <div className={styles['thanks-top']}>Thank you for the email</div>
+
+        <div className={styles['thanks-bottom']}>I'll be back to you shortly!</div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.contactPage}>
       {error && (
@@ -67,81 +149,7 @@ function ContactPage() {
         ></ErrorModal>
       )}
       <div className={styles.image}></div>
-        {!isSent && 
-        <Card className={styles.contact_me}>
-        <div className={styles.contact_me_section}>
-          <form
-            onSubmit={onSubmitHanlder}
-            encType="text/plain"
-            className={styles.form_main_box}
-          >
-            <div className={styles.form_section}>
-              <h1>Contact Me</h1>
-              <label className={styles.name} htmlFor="name">
-                Full Name
-              </label>
-              <input
-                className={styles.name_input}
-                type="text"
-                name="name"
-                id="name"
-                ref={nameRef}
-                onChange={onChangeHandler}
-              ></input>
-              <label className={styles.email} htmlFor="email">
-                Email
-              </label>
-              <input
-                className={styles.email_input}
-                ref={emailRef}
-                type="email"
-                name="email"
-                id="email"
-                onChange={onChangeHandler}
-              ></input>
-              <label className={styles.message} htmlFor="message">
-                Message
-              </label>
-              <textarea
-                className={styles.message_input}
-                type="text"
-                name="message"
-                id="message"
-                placeholder="Any questions?"
-                ref={messageRef}
-                onChange={onChangeHandler}
-              ></textarea>
-            </div>
-            <div>
-              <button className={styles.button_section} type="submit">
-                Send
-              </button>
-            </div>
-          </form>
-          <div className={styles.info_section}>
-            <div className={styles.info}>
-              <div className={styles.info_email}>
-                <h2>Email</h2>
-                <div className={styles.info_email_text}>{email}</div>
-              </div>
-
-              <div className={styles.info_location}>
-                <h2>Based in</h2>
-                <div className={styles.info_location_text}>
-                  <div>Montreal</div>
-                  <div>Quebec</div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.media} onClick={socialMediaHandler}>
-              <Card className={styles.contactPage_media}>LinkedIn</Card>
-              <Card className={styles.contactPage_media}>Github</Card>
-            </div>
-          </div>
-        </div>
-      </Card>
-        }
-      {isSent && <div>Thank you for the message, I'll be back to you shortly</div>}
+      <Card className={styles.contact_me}>{content}</Card>
     </div>
   );
 }
